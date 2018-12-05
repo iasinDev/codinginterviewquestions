b13ce8983c70b846fe6c43cc47ad26b7daa801cd
Mathias Arens <mathias.arens@googlemail.com>
2018-12-05 12:14:47 -0800

    Small improvements to longest necklace

--- Longest Necklace/Solution.py
+++ Longest Necklace/Solution.py
@@ -3,23 +3,22 @@
 def solution(A):
     # write your code in Python 3.6
     max_length = 0
+    visited = [False] * len(A)
     for i in range(0, len(A)):
-        length = test(A, i, {}, 0)
-        if length > max_length:
-            max_length = length
+        max_length = max(max_length, test(A, i, visited, 0))
     return max_length
 
 def test(A,i,visited,length):
-    if i in visited:
+    if visited[i]:
         return length
     else:
-        visited[i] = 'true'
+        visited[i] = True
         if A[i]>=len(A):
             return length
         else:
             return test(A, A[i], visited, length+1)
 
-print(solution([5,4,0,3,1,6,2]))
-print(solution([]))
-print(solution([0]))
-print(solution([1]))
+print(solution([5,4,0,3,1,6,2]) == 4)
+print(solution([]) == 0)
+print(solution([0]) == 1)
+print(solution([1]) == 0)
